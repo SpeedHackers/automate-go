07e0c33ccf729087e9ae5a67087e05674d5e2223
Josh Chase <jcjoshuachase@gmail.com>
2014-11-20 13:03:44 -0500

    dockerfile

---
+++ docker/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ ! -v OH_URL ]
+then
+	echo Need OH_URL environment variable
+	exit 1
+fi
+
+tls=""
+cert=""
+key=""
+if [ -v OH_TLS ]
+then
+	if [ $OH_TLS == "true" ]
+	then
+		tls="-tls"
+		cert="/etc/automated/cert.pem"
+		key="/etc/automated/key.pem"
+	fi
+fi
+
+/go/bin/automated -ohurl $OH_URL $tls $cert $key
